46b2c606819d72282302e1dd25a71b971649954d
JakobBe <jakob@bethmann.de>
2020-12-01 19:35:40 +0100

    add spring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,76 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, Animated} from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
 
 
-const Footer = () => {
+const Footer = () => { 
+  const [isOpen, setIsOpen ] = useState(() => false);
+
+  const toggleFooter = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  }
+
   const onCategoriesPress = () => {
-      Actions.categories({ type: ActionConst.REPLACE })
-    }
+    Actions.categories({ type: ActionConst.REPLACE });
+  }
 
-    const onHomePress = () => {
-      Actions.gestureIndex({ type: ActionConst.REPLACE })
-    }
+  const onHomePress = () => {
+    Actions.gestureIndex({ type: ActionConst.REPLACE });
+  }
 
+
+  if (isOpen) {
+    return (
+      <TouchableOpacity onPress={toggleFooter} style={styles.footerContainer}>
+        <View style={styles.footerItemWrapper}>
+          <TouchableOpacity onPress={onCategoriesPress}>
+            <Text style={styles.footerItemTitle}>
+              Kategorien
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onHomePress}>
+            <Text style={styles.footerItemTitle}>
+              Gebärden
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    );
+  }
   return (
-    <View style={styles.footerContainer}>
-      <View style={styles.footerItemWrapper}>
-        <TouchableOpacity onPress={onCategoriesPress}>
-          <Text style={styles.footerItemTitle}>
-            Kategorien
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onHomePress}>
-          <Text style={styles.footerItemTitle}>
-            Gebärden
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <TouchableOpacity onPress={toggleFooter} style={styles.footerContainerClosed}>
+      <Text>
+        |||
+      </Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = {
+  footerContainerClosed: {
+    backgroundColor: '#F7E3EA',
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    position: 'absolute',
+    shadowColor: 'black',
+    position: 'absolute',
+    bottom: 10,
+    margin: 10,
+    right: 10,
+    flex: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 13.22,
+
+    elevation: 10,
+  },
+
   footerContainer: {
     backgroundColor: '#F7E3EA',
     height: 60,
@@ -48,7 +88,6 @@ const styles = {
     },
     shadowOpacity: 0.32,
     shadowRadius: 13.22,
-
     elevation: 10,
   },
 
